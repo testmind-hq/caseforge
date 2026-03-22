8faf572ee23fc9ea9f6d89867bfae8c562e2a55d
yuchou87 <yuchou87@gmail.com>
2026-03-22 11:13:15 +0800

    feat: wire gen command with real spec loader, methodology engine, and Hurl renderer

---
+++ cmd/gen_integration_test.go
@@ -0,0 +1,31 @@
+// cmd/gen_integration_test.go
+//go:build integration
+
+package cmd
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenCommandWithPetstoreSpec(t *testing.T) {
+	outDir := t.TempDir()
+	genSpec = "../testdata/petstore.yaml"
+	genOutput = outDir
+	genNoAI = true
+	genFormat = "hurl"
+
+	err := runGen(nil, nil)
+	require.NoError(t, err)
+
+	// index.json must exist
+	indexPath := filepath.Join(outDir, "index.json")
+	assert.FileExists(t, indexPath)
+
+	// At least one .hurl file
+	hurls, _ := filepath.Glob(filepath.Join(outDir, "*.hurl"))
+	assert.NotEmpty(t, hurls)
+}
